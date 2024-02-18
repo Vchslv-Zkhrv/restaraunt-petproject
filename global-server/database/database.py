37a0a8e74245f2d75f8ba66a3f05f89cd6da69e2
Vchslv-Zkhrv <vchslv.zkhrv@gmail.com>
2024-02-18 16:15:30 +0300

    root creation

--- global-server/database/database.py
+++ global-server/database/database.py
@@ -1,5 +1,6 @@
 import logging as _logging
 
+from sqlalchemy.pool import NullPool as _NullPoll
 from sqlalchemy.ext.asyncio import AsyncSession as _Session
 from sqlalchemy.ext.asyncio import create_async_engine as _create_async_engine
 from sqlalchemy.ext.declarative import declarative_base as _declarative_base
@@ -7,9 +8,21 @@ from sqlalchemy.orm import sessionmaker as _sessionmaker
 
 from config import env as _env
 
-engine = _create_async_engine(_env.db_connect_url, echo=_env.mode != "production")
+engine = _create_async_engine(
+    _env.db_connect_url,
+    echo=_env.mode != "production",
+    poolclass=_NullPoll,
+    pool_pre_ping=True
+)
 Base = _declarative_base()
-AsyncSession = _sessionmaker(engine, class_=_Session, expire_on_commit=False)  # pyright: ignore
+
+
+AsyncSession = _sessionmaker(
+    engine,  # pyright: ignore
+    class_=_Session,
+    expire_on_commit=False,
+
+)
 
 
 if _env.mode != "production":
--- global-server/database/endpoints.py
+++ global-server/database/endpoints.py
@@ -4,24 +4,41 @@ Database CRUDS endpoints.
 All interaction with database must be procceeded here (not in models.py)
 """
 
+import typing as _t
+import sys as _sys
+
 from sqlalchemy.ext.asyncio import AsyncSession as _AsyncSession
 
 from . import database as _db
-from . import models as _models
+from . import models as _m
+from .types_ import exceptions as _errors
 
 
 async def get_session() -> _AsyncSession:  # pyright: ignore
     """Creates db session, yields it and closes after use"""
     async with _db.AsyncSession() as session:  # pyright: ignore
         yield session  # pyright: ignore
+        session.close()
 
 
-async def init_models():
+async def init_models() -> _t.NoReturn:
     """Drops all old tables and creates new from models.py"""
     async with _db.engine.begin() as conn:
         await conn.run_sync(_db.Base.metadata.drop_all)
         await conn.run_sync(_db.Base.metadata.create_all)
+    await create_root()
 
 
-async def spam():
-    return _models
+async def create_root() -> _t.NoReturn:
+    """Creates root actor"""
+    async with _db.AsyncSession() as se:  # pyright: ignore
+        se: _AsyncSession
+        if await se.get(_m.Actor, 1):
+            raise _errors.ActorExistsError("Cannot create root: Actor with id=1 already exists")
+        root = _m.Actor()
+        se.add(root)
+        await se.commit()
+        await se.refresh(root)
+        if root.id != 1:
+            raise _errors.ActorCreationError("Failed: root actor id != 1")
+    _sys.exit(0)
